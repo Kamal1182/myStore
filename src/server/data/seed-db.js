0b3d30c77c8604ace4c2956a0134bbf021b7c946
kamal1182 <mkamal1182@gmail.com>
2022-08-21 19:57:54 +0200

    backend and database are up and running

--- src/server/data/seed-db.js
+++ src/server/data/seed-db.js
@@ -3,24 +3,26 @@ require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 const bcrypt = require('bcrypt');
 
-const users = require('./users');
-const contacts = require('./contacts');
+const users = require('./users.json');
+const products = require('./products.json');
 require('dotenv').config();
 
 
 
 function seedCollection(collectionName, initialRecords) {
 
-  MongoClient.connect(process.env.DB_CONN, (err, db) => {
+  MongoClient.connect(process.env.DB_CONN, (err, client) => {
+    var db = client.db('myStore');
+    
     console.log('connected to mongodb...');
 
     const collection = db.collection(collectionName);
 
     collection.remove();  
-
+    
     initialRecords.forEach((item) => {
       if (item.password) {
-        item.password = bcrypt.hashSync(item.password, process.env.hashSalt);
+        item.password = bcrypt.hashSync(item.password, parseInt(process.env.hashSalt))
       }
     });
 
@@ -29,7 +31,7 @@ function seedCollection(collectionName, initialRecords) {
     collection.insertMany(initialRecords, (err, result) => {
       console.log(`${result.insertedCount} records inserted.`);
       console.log('closing connection...');
-      db.close();
+      client.close();
       console.log('done.');
     });
 
@@ -38,4 +40,4 @@ function seedCollection(collectionName, initialRecords) {
 
 
 seedCollection('users', users);
-seedCollection('contacts', contacts);
+seedCollection('products', products);
